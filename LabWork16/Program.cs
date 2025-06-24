76ed7dcb317c6e115c5dc8d3a45b713ac4e7c517
shipilka <chipashipilova@gmail.com>
2025-06-24 21:32:03 +0300

    Add files via upload

---
+++ LabWork16/Program.cs
@@ -0,0 +1,35 @@
+﻿// Task1/Program.cs
+using System;
+using System.ComponentModel;
+using Task1;
+
+namespace Task1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                User user = new User { Login = "initialLogin", Password = "initialPassword" };
+
+                user.PropertyChanged += (sender, e) =>
+                {
+                    Console.WriteLine($"Свойство '{e.PropertyName}' было изменено.");
+                };
+
+                user.Login = "newLogin";
+                user.Password = "newPassword";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Произошла ошибка: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("\nНажмите любую клавишу для завершения...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ LabWork16/User.cs
@@ -0,0 +1,45 @@
+﻿// Task1/User.cs
+using System;
+using System.ComponentModel;
+
+namespace Task1
+{
+    public class User : INotifyPropertyChanged
+    {
+        private string login;
+        private string password;
+
+        public string Login
+        {
+            get => login;
+            set
+            {
+                if (login != value)
+                {
+                    login = value;
+                    OnPropertyChanged(nameof(Login));
+                }
+            }
+        }
+
+        public string Password
+        {
+            get => password;
+            set
+            {
+                if (password != value)
+                {
+                    password = value;
+                    OnPropertyChanged(nameof(Password));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
